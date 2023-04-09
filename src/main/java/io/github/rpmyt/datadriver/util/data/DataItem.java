06809a9dfc957979910e06ef943526a8621b1ae5
PrismaticYT <45874270+RPMYT@users.noreply.github.com>
2023-04-09 04:35:56 +0000

    It begins.

---
+++ src/main/java/io/github/rpmyt/datadriver/util/data/DataItem.java
@@ -0,0 +1,10 @@
+package io.github.rpmyt.datadriver.util.data;
+
+import com.google.gson.JsonObject;
+
+public class DataItem {
+    public String name;
+    public String identifier;
+    public String type;
+    public JsonObject data;
+}
---
+++ src/main/java/io/github/rpmyt/datadriver/util/data/Template.java
@@ -0,0 +1,84 @@
+package io.github.rpmyt.datadriver.util.data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+import io.github.rpmyt.datadriver.DatadriverInit;
+
+import java.util.Map.Entry;
+
+public class Template extends DataItem {
+    public Class<?> superclass;
+    public HashMap<String, ArrayList<String>> methods = new HashMap<>();
+    public ArrayList<String> requiredMods = new ArrayList<>();
+    public HashMap<String, String> placeholders = new HashMap<>();
+
+    public Template(DataItem parent) {
+        this.data = parent.data;
+        this.name = parent.name;
+        this.identifier = parent.identifier;
+        this.type = parent.type;
+        this.init();
+    }
+    
+    public void init() {
+        for (Entry<String,JsonElement> entry : this.data.entrySet()) {
+            switch (entry.getKey()) {
+                case "mods": {
+                    if (entry.getValue().isJsonArray()) {
+                        JsonArray modlist = entry.getValue().getAsJsonArray();
+                        modlist.forEach(mod -> {
+                            if (mod.isJsonPrimitive() && mod.getAsJsonPrimitive().isString()) {
+                                requiredMods.add(mod.getAsJsonPrimitive().getAsString());
+                            }
+                        });
+                    }
+                    break;
+                }
+
+                case "class": {
+                    if (entry.getValue().isJsonPrimitive() && entry.getValue().getAsJsonPrimitive().isString()) {
+                        String desc = entry.getValue().getAsJsonPrimitive().getAsString();
+                        try {
+                            this.superclass = Class.forName(desc);
+                        } catch (ClassNotFoundException exception) {
+                            DatadriverInit.LOGGER.error("Unable to load superclass '" + desc + "'!");
+                        }
+                    }
+                    break;
+                }
+
+                case "methods": {
+                    if (entry.getValue().isJsonObject()) {
+                        entry.getValue().getAsJsonObject().entrySet().forEach(method -> {
+                            if (method.getValue().isJsonArray()) {
+                                ArrayList<String> bytecode = new ArrayList<>();
+                                method.getValue().getAsJsonArray().forEach(instruction -> {
+                                    if (instruction.isJsonPrimitive() && instruction.getAsJsonPrimitive().isString()) {
+                                        bytecode.add(instruction.getAsJsonPrimitive().getAsString());
+                                    }
+                                });
+                                methods.put(entry.getKey(), bytecode);
+                            }
+                        });
+                    }
+                    break;
+                }
+
+                case "placeholders": {
+                    if (entry.getValue().isJsonObject()) {
+                        entry.getValue().getAsJsonObject().entrySet().forEach(placeholder -> {
+                            if (placeholder.getValue().isJsonPrimitive() && placeholder.getValue().getAsJsonPrimitive().isString()) {
+                                placeholders.put(entry.getKey(), placeholder.getValue().getAsJsonPrimitive().getAsString());
+                            }
+                        });
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
